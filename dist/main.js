4959714695a40dab5959246843724e484d1b0ca2
hda8821791 <hda8821791@gmail.com>
2021-09-24 00:57:18 +0800

    test

--- dist/main.js
+++ dist/main.js
@@ -13,7 +13,7 @@ async function bootstrap() {
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, options);
     swagger_1.SwaggerModule.setup('api-docs', app, document);
-    const PORT = parseInt(process.env.PORT) | 5000;
+    const PORT = parseInt(process.env.PORT) || 5000;
     await app.listen(PORT);
     console.log(`http://localhost:${PORT}/api-docs`);
 }
